afc42a3ee411f7e3a0b88c518c6146f40716cd0d
salrey <tekdehn@gmail.com>
2021-11-18 12:23:05 -0500

    Add how much each dino eats

--- src/components/SelectedDinosaur.js
+++ src/components/SelectedDinosaur.js
@@ -1,11 +1,19 @@
 const SelectedDino = (props) => {
   const { image, name, weight, carnivore } = props.dino;
+  const foodCost = Math.floor(weight / 5) + 1 || 0;
+  //use objects to check if the math above works for each dino
+  // console.log({dino: props.dino})
+  // console.log({foodCost: foodCost})
+
+  // Only display this section if dino is selected, so since dino evaluates to an object that starts out with empty name key, check if name even exists 
+  const eats = props.dino.name && <p>Eats: {foodCost} {carnivore ? "goats" : "trees"}</p> 
 
   return (
     <div className="selected-dino">
       <h3>Currently Selected Dinosaur</h3>
       <img className="dino-image" src={image} alt="Selected dinosaur image" />
       <p>{name || "No Dinosaur Selected"}</p>
+      {eats}
     </div>
   );
 };
